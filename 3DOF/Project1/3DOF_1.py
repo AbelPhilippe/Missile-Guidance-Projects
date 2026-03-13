5969d091f9d66f9ba397156f91330b152c4bda32
AbelPhilippe <abelphilipe@live.com>
2026-03-13 05:05:33 -0300

    derivates removed

--- 3DOF/Project1/3DOF_1.py
+++ 3DOF/Project1/3DOF_1.py
@@ -10,6 +10,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import scipy as sp
 
 # ==============================================================================
 #                              Physical parameters
